eb289c2e4c66c675e081b2b2015235181bc9a616
thomaspro <thomaspro@thomaspro.nazwa.pl>
2018-03-06 06:59:43 +0100

    Working weather

--- app/src/main/java/com/example/peethr/wsbtest/models/connection/HttpConnection.java
+++ app/src/main/java/com/example/peethr/wsbtest/models/connection/HttpConnection.java
@@ -2,11 +2,14 @@ package com.example.peethr.wsbtest.models.connection;
 
 import android.net.ConnectivityManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.peethr.wsbtest.models.weather.CurrentWeather;
 import com.example.peethr.wsbtest.models.weather.GetCurrentDetails;
+import com.example.peethr.wsbtest.models.weather.Globals;
 
 import org.json.JSONException;
+
 
 import java.io.IOException;
 
@@ -16,11 +19,13 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import static java.lang.Math.floor;
+
 public class HttpConnection {
+
 
     private CurrentWeather currentWeather;
     private CheckInternetConnection checkInternetConnection = new CheckInternetConnection();
-
 
     public CurrentWeather darkSkyConnection(String forecastUrl, ConnectivityManager manager)
     {
@@ -44,9 +49,12 @@ public class HttpConnection {
                         Log.v("HttpConnection", weatherData);
                         if (response.isSuccessful()) {
                             GetCurrentDetails getCurrentDetails = new GetCurrentDetails();
-                           currentWeather = getCurrentDetails.getCurrentDetails(weatherData);
+                            currentWeather = getCurrentDetails.getCurrentDetails(weatherData);
 
-                           Log.d("Elo", String.valueOf(currentWeather.getTemperature()));
+                            Log.d("Elo", String.valueOf(currentWeather.getTemperature()));
+                            Globals g = Globals.getInstance();
+                            g.setTemperature((int)floor(currentWeather.getTemperature()));
+                            g.setSummary(currentWeather.getSummary());
 
                         } else {
 
@@ -66,4 +74,8 @@ public class HttpConnection {
         }
         return currentWeather;
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/peethr/wsbtest/models/weather/Globals.java
@@ -0,0 +1,33 @@
+package com.example.peethr.wsbtest.models.weather;
+
+public class Globals{
+    private static Globals instance;
+
+    // Global variable
+    private int temperature;
+    private String summary;
+
+    // Restrict the constructor from being instantiated
+    private Globals(){}
+
+    public void setTemperature(int temperature){
+        this.temperature = temperature;
+    }
+    public int getTemperature(){
+        return this.temperature;
+    }
+
+    public void setSummary(String summary){
+        this.summary = summary;
+    }
+    public String getSummary(){
+        return this.summary;
+    }
+
+    public static synchronized Globals getInstance(){
+        if(instance==null){
+            instance=new Globals();
+        }
+        return instance;
+    }
+}
