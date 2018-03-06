b6c51934ececf28c2c9cbfa156167af885f66f8d
deVant <37074801+devantLab@users.noreply.github.com>
2018-03-06 01:22:58 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/peethr/wsbtest/Models/NotificationAlertManager.java
@@ -0,0 +1,21 @@
+package com.example.peethr.wsbtest.Models;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+
+
+/**
+ * Created by thomas on 05.03.18.
+ */
+
+public class NotificationAlertManager {
+    static int id;
+    private NotificationManager notificationManager;
+
+
+    public NotificationAlertManager(Object systemService, Notification alert){
+        notificationManager = (NotificationManager) systemService;
+
+        notificationManager.notify(id++, alert);
+    }
+}
---
+++ app/src/main/java/com/example/peethr/wsbtest/Models/NotificationCreator.java
@@ -0,0 +1,43 @@
+package com.example.peethr.wsbtest.Models;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+/**
+ * Created by thomas on 05.03.18.
+ */
+
+public class NotificationCreator {
+    private Intent intent;
+    private PendingIntent pIntent;
+    private NotificationAlertManager notificationAlertManager;
+    private Notification alert;
+    private Context context;
+    public NotificationCreator(Context context, Class activityClass){
+
+        this.context = context;
+        intent = new Intent(context, activityClass);
+        pIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+    }
+    public void create(String title, String text, String ticker, int smallIcon, Bitmap largeIcon){
+        alert = new NotificationAlert(context, "alert")
+                        .setContentTitle(title)
+                        .setContentText(text)
+                        .setTicker(ticker)
+                        .setSmallIcon(smallIcon)
+                        .setLargeIcon(largeIcon)
+                        .setAutoCancel(true)
+                        .setContentIntent(pIntent)
+                        .build();
+        notificationAlertManager = new NotificationAlertManager(context.getSystemService(NOTIFICATION_SERVICE), alert);
+
+    }
+
+}
