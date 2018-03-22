700931d383b44ddc42264891036ead49454e0c7d
goracy <goracykubekwiniary@gmail.com>
2018-03-22 16:15:23 +0100

    UniversityFragmentButtons

---
+++ app/src/main/java/com/example/peethr/wsbtest/presenters/MapActivity.java
@@ -0,0 +1,15 @@
+package com.example.peethr.wsbtest.presenters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.peethr.wsbtest.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
