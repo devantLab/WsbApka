eafa88394aff4db5c2f0f8f66ab4ec7fa7cea2d8
goracy <goracykubekwiniary@gmail.com>
2018-04-04 19:04:06 +0200

    add drawer

--- app/src/main/java/com/example/peethr/wsbtest/fragments/FragmentInteractionListener.java
+++ app/src/main/java/com/example/peethr/wsbtest/fragments/FragmentInteractionListener.java
@@ -1,9 +1,13 @@
 package com.example.peethr.wsbtest.fragments;
 
+import android.view.MenuItem;
+
 /**
  * Created by thomas on 09.03.18.
  */
 
 public interface FragmentInteractionListener extends DashboardFragment.OnFragmentInteractionListener, EventFragment.OnFragmentInteractionListener, InfoFragment.OnFragmentInteractionListener, UniversityFragment.OnFragmentInteractionListener
 {
+    @SuppressWarnings("StatementWithEmptyBody")
+    boolean onNavigationItemSelected(MenuItem item);
 }
--- app/src/main/java/com/example/peethr/wsbtest/presenters/MainActivity.java
+++ app/src/main/java/com/example/peethr/wsbtest/presenters/MainActivity.java
@@ -1,64 +1,69 @@
 package com.example.peethr.wsbtest.presenters;
 
-import android.graphics.Color;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.peethr.wsbtest.R;
 import com.example.peethr.wsbtest.fragments.FragmentInteractionListener;
 import com.example.peethr.wsbtest.fragments.factory.FragmentFacotry;
 import com.example.peethr.wsbtest.models.data.weather.Globals;
 
-public class MainActivity extends AppCompatActivity implements FragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements FragmentInteractionListener, NavigationView.OnNavigationItemSelectedListener {
 
     Globals g = Globals.getInstance();
 
     private FragmentFacotry fragmentFacotry;
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
+
     private SectionsPagerAdapter mSectionsPagerAdapter;
-    private Button eventButton;
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
+
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggleButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggleButton = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggleButton);
+        toggleButton.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabLayout =  findViewById(R.id.tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
@@ -69,14 +74,45 @@ public class MainActivity extends AppCompatActivity implements FragmentInteracti
 
 
 
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     public void onFragmentInteraction(Uri uri) {
 
     }
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
@@ -97,12 +133,6 @@ public class MainActivity extends AppCompatActivity implements FragmentInteracti
             return 4;
         }
     }
-
-    @Override
-    public void onBackPressed() {
-        moveTaskToBack(true);
-    }
-
 
     public void selectFragment(int position){
         mViewPager.setCurrentItem(position, true);
