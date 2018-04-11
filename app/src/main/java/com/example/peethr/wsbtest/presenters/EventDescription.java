1b9dad1d7de62c13f6f949f6bcc8c76e6c8a0f43
goracy <goracykubekwiniary@gmail.com>
2018-04-11 17:55:08 +0200

    add close on swipe in event&place description

--- app/src/main/java/com/example/peethr/wsbtest/presenters/EventDescription.java
+++ app/src/main/java/com/example/peethr/wsbtest/presenters/EventDescription.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,6 +28,9 @@ public class EventDescription extends AppCompatActivity {
     private TextView eventDescriptionDescription;
     private ImageView eventDescriptionImage;
     private Button eventPageButton;
+
+    private float y1, y2;
+    static final int MIN_DISTANCE = 150;
 
     private Toolbar toolbar;
 
@@ -97,6 +101,26 @@ public class EventDescription extends AppCompatActivity {
     protected void onPause() {
         g.setShowNewstEvent(false);
         super.onPause();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                y2 = event.getY();
+                float deltaY = y2 - y1;
+                if (Math.abs(deltaY) > MIN_DISTANCE)
+                {
+                    onBackPressed();
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
